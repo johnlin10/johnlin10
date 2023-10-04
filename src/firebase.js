3cbc19746ba818f463aff7cb3bc9e918792bf642
Johnolin10 <101758259+johnlin10@users.noreply.github.com>
2023-10-04 14:28:18 +0800

    新增支持在音頻播放控制台顯示歌曲資訊

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore'
+import { getStorage, ref, getDownloadURL } from 'firebase/storage'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCON4HIstOfS63WngxYHyHoFciym74q2kM',
   authDomain: 'johnlin10-web.firebaseapp.com',
@@ -19,3 +17,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+export async function getFileUrl(path) {
+  const storageRef = ref(storage, path)
+  const url = await getDownloadURL(storageRef)
+  return url
+}
